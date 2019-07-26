7be13c9d368f2384fe5808753b153a0944218a5a
Jan Niklas Rose <janniklas.rose@gmail.com>
2019-07-26 13:01:09 +0100

    Added and integrated fast binary stl reader
    Version 1.0.0.0
    From: https://uk.mathworks.com/matlabcentral/fileexchange/29906-binary-stl-file-reader

--- stlRead.m
+++ stlRead.m
@@ -1,16 +1,25 @@
-function [v, f, n, name] = stlRead(fileName)
+function [v, f, n, name] = stlRead(fileName,legacy)
 %STLREAD reads any STL file not depending on its format
 %V are the vertices
 %F are the faces
 %N are the normals
 %NAME is the name of the STL object (NOT the name of the STL file)
+%LEGACY is a boolean that, if true, uses the legacy reader for binary files
+
+if nargin < 2
+  legacy = false();
+end
 
 % read content
 format = stlGetFormat(fileName);
 if strcmp(format,'ascii')
   [v,f,n,name] = stlReadAscii(fileName);
 elseif strcmp(format,'binary')
-  [v,f,n,name] = stlReadBinary(fileName);
+  if legacy
+    [v,f,n,name] = stlReadBinary(fileName);
+  else
+    [v,f,n,~,name] = stlReadBinary_fast(fileName);
+  end
 end
 
 % slim the data (delete duplicated vertices)
